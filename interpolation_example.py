c9eb3a59193bf125521994f5fa892d312e74438e
Enbo Zhou <enbozhou@126.com>
2023-02-08 23:55:07 -0800

    Get the difference of CHIRPS and PRISM

--- interpolation_example.py
+++ interpolation_example.py
@@ -10,13 +10,17 @@ prism_data_dir  = './PRISM_annual_daily_ppt_4km_2018.nc'
 chirps_data = xr.open_dataset(chirps_data_dir)
 prism_data = xr.open_dataset(prism_data_dir)
 
+# Get the first day precipitation
 prism_first_day = prism_data.ppt[121, :, :]
 chirps_first_day = chirps_data.precip[121, :, :]
 print(prism_first_day)
 print(chirps_first_day)
 
-prism_interpolated = prism_data.ppt[0, :, :].interp_like(chirps_data)
-
-prism_interpolated.plot.pcolormesh()
+# interpolate chirps to prism
+chirps_interpolated = chirps_first_day.interp_like(prism_first_day)
+# Get the difference
+diff = chirps_interpolated - prism_first_day
+# Difference visualization 
+diff.plot.pcolormesh()
 
 plt.show()
