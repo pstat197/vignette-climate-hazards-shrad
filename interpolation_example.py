a48bb0f4294b07222588c378b952555e755321fb
Enbo Zhou <enbozhou@126.com>
2023-02-09 11:37:02 -0800

    A small modification of the comments.

--- interpolation_example.py
+++ interpolation_example.py
@@ -10,7 +10,7 @@ prism_data_dir  = './PRISM_annual_daily_ppt_4km_2018.nc'
 chirps_data = xr.open_dataset(chirps_data_dir)
 prism_data = xr.open_dataset(prism_data_dir)
 
-# Get the first day precipitation
+# Get the 121 day's precipitation
 prism_first_day = prism_data.ppt[121, :, :]
 chirps_first_day = chirps_data.precip[121, :, :]
 print(prism_first_day)
